8ef81a4d4832ad1d7a74f02d9f10d7e5c099964c
Ranielly <ranielyypatrcia98@gmail.com>
2024-04-02 14:30:08 -0300

    comentarios
    coments para explicar o cod

--- invertendo.py
+++ invertendo.py
@@ -1,8 +1,9 @@
 def inverter_string(s):
-    inverted = ""
-    for i in range(len(s) - 1, -1, -1):
-        inverted += s[i]
-    return inverted
+    inverted = ""  # Uma string vazia para armazenar a string invertida
+
+    for i in range(len(s) - 1, -1, -1):# Loop que itera sobre os caracteres da string original de trás para frente
+        inverted += s[i]  # Adiciona o caractere atual à string invertida
+    return inverted  
 
 string_original = "Hello, world!"
 string_invertida = inverter_string(string_original)
